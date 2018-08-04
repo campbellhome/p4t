82e49b2d715cd966dfb0487dd0aae9b7bb8b24da
Matt Campbell <campbellhome@gmail.com>
2018-08-04 11:11:23 -0700

    remove using namespace ImGui;

--- src/p4t/ui_config.cpp
+++ src/p4t/ui_config.cpp
@@ -8,7 +8,6 @@
 #include "bb_array.h"
 #include "bb_string.h"
 
-using namespace ImGui;
 void QueueUpdateDpiDependentResources();
 
 static config_t *s_uiConfig;
@@ -53,7 +52,7 @@ void UIConfig_ApplyColorscheme(config_t *config)
 	if(!config) {
 		config = &g_config;
 	}
-	Style_Reset();
+	ImGui::Style_Reset();
 	const char *colorscheme = sb_get(&config->colorscheme);
 	if(!strcmp(colorscheme, "Visual Studio Dark")) {
 		ImGui::StyleColorsVSDark();
@@ -74,14 +73,14 @@ void UIConfig_Update(config_t *config)
 {
 	float startY = ImGui::GetItemsLineHeightWithSpacing();
 	ImGuiIO &io = ImGui::GetIO();
-	SetNextWindowSize(ImVec2(io.DisplaySize.x, io.DisplaySize.y - startY), ImGuiSetCond_Always);
-	SetNextWindowPos(ImVec2(0, startY), ImGuiSetCond_Always);
+	ImGui::SetNextWindowSize(ImVec2(io.DisplaySize.x, io.DisplaySize.y - startY), ImGuiSetCond_Always);
+	ImGui::SetNextWindowPos(ImVec2(0, startY), ImGuiSetCond_Always);
 
-	if(Begin("Configuration", &s_configOpen, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoTitleBar)) {
+	if(ImGui::Begin("Configuration", &s_configOpen, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoTitleBar)) {
 		if(ImGui::CollapsingHeader("Interface", ImGuiTreeNodeFlags_DefaultOpen)) {
-			InputFloat("Double-click seconds", &s_uiConfig->doubleClickSeconds);
-			Checkbox("Single instance check", &s_uiConfig->singleInstanceCheck);
-			Checkbox("Single instance prompt", &s_uiConfig->singleInstancePrompt);
+			ImGui::InputFloat("Double-click seconds", &s_uiConfig->doubleClickSeconds);
+			ImGui::Checkbox("Single instance check", &s_uiConfig->singleInstanceCheck);
+			ImGui::Checkbox("Single instance prompt", &s_uiConfig->singleInstancePrompt);
 
 			int colorschemeIndex = -1;
 			for(int i = 0; i < BB_ARRAYSIZE(s_colorschemes); ++i) {
@@ -97,101 +96,101 @@ void UIConfig_Update(config_t *config)
 					UIConfig_ApplyColorscheme(s_uiConfig);
 				}
 			}
-			Checkbox("DPI Aware", &s_uiConfig->dpiAware);
+			ImGui::Checkbox("DPI Aware", &s_uiConfig->dpiAware);
 			if(ImGui::IsItemHovered()) {
 				ImGui::SetTooltip("Requires restart.  Default font is not recommended if DPI Aware.");
 			}
 		}
 		if(ImGui::CollapsingHeader("Font", ImGuiTreeNodeFlags_DefaultOpen)) {
-			BeginGroup();
-			PushID("UIFont");
-			Checkbox("Custom UI Font", &s_uiConfig->uiFontConfig.enabled);
+			ImGui::BeginGroup();
+			ImGui::PushID("UIFont");
+			ImGui::Checkbox("Custom UI Font", &s_uiConfig->uiFontConfig.enabled);
 			if(s_uiConfig->uiFontConfig.enabled) {
 				ImGui::AlignFirstTextHeightToWidgets();
 				ImGui::TextUnformatted("Font size:");
 				ImGui::SameLine();
 				int val = (int)s_uiConfig->uiFontConfig.size;
-				InputInt("##size", &val, 1, 10);
+				ImGui::InputInt("##size", &val, 1, 10);
 				val = BB_CLAMP(val, 1, 1024);
 				s_uiConfig->uiFontConfig.size = (u32)val;
-				TextUnformatted("Path:");
-				SameLine();
-				PushItemWidth(300.0f * g_config.dpiScale);
+				ImGui::TextUnformatted("Path:");
+				ImGui::SameLine();
+				ImGui::PushItemWidth(300.0f * g_config.dpiScale);
 				ImGuiInputTextFlags flags = ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue;
-				InputText("##path", &s_uiConfig->uiFontConfig.path, 1024, flags);
-				PopItemWidth();
+				ImGui::InputText("##path", &s_uiConfig->uiFontConfig.path, 1024, flags);
+				ImGui::PopItemWidth();
 			}
-			PopID();
-			EndGroup();
-			SameLine();
-			BeginGroup();
-			PushID("LogFont");
-			Checkbox("Custom Log Font", &s_uiConfig->logFontConfig.enabled);
+			ImGui::PopID();
+			ImGui::EndGroup();
+			ImGui::SameLine();
+			ImGui::BeginGroup();
+			ImGui::PushID("LogFont");
+			ImGui::Checkbox("Custom Log Font", &s_uiConfig->logFontConfig.enabled);
 			if(s_uiConfig->logFontConfig.enabled) {
 				ImGui::AlignFirstTextHeightToWidgets();
 				ImGui::TextUnformatted("Font size:");
 				ImGui::SameLine();
 				int val = (int)s_uiConfig->logFontConfig.size;
-				InputInt("size", &val, 1, 10);
+				ImGui::InputInt("size", &val, 1, 10);
 				val = BB_CLAMP(val, 1, 1024);
 				s_uiConfig->logFontConfig.size = (u32)val;
-				Text("Path:");
-				SameLine();
-				PushItemWidth(300.0f * g_config.dpiScale);
+				ImGui::Text("Path:");
+				ImGui::SameLine();
+				ImGui::PushItemWidth(300.0f * g_config.dpiScale);
 				ImGuiInputTextFlags flags = ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue;
-				InputText("##path", &s_uiConfig->logFontConfig.path, 1024, flags);
-				PopItemWidth();
+				ImGui::InputText("##path", &s_uiConfig->logFontConfig.path, 1024, flags);
+				ImGui::PopItemWidth();
 			}
-			EndGroup();
-			PopID();
+			ImGui::EndGroup();
+			ImGui::PopID();
 		}
 		if(ImGui::CollapsingHeader("Diff", ImGuiTreeNodeFlags_DefaultOpen)) {
-			PushID("Diff");
-			Checkbox("External Program", &s_uiConfig->diff.enabled);
+			ImGui::PushID("Diff");
+			ImGui::Checkbox("External Program", &s_uiConfig->diff.enabled);
 			if(s_uiConfig->diff.enabled) {
-				Text("Path:");
-				SameLine();
-				PushItemWidth(600.0f * g_config.dpiScale);
+				ImGui::Text("Path:");
+				ImGui::SameLine();
+				ImGui::PushItemWidth(600.0f * g_config.dpiScale);
 				ImGuiInputTextFlags flags = ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue;
-				InputText("##path", &s_uiConfig->diff.path, 1024, flags);
-				PopItemWidth();
-				Text("Arguments:");
-				SameLine();
-				PushItemWidth(300.0f * g_config.dpiScale);
-				InputText("##args", &s_uiConfig->diff.args, 1024, flags);
-				PopItemWidth();
+				ImGui::InputText("##path", &s_uiConfig->diff.path, 1024, flags);
+				ImGui::PopItemWidth();
+				ImGui::Text("Arguments:");
+				ImGui::SameLine();
+				ImGui::PushItemWidth(300.0f * g_config.dpiScale);
+				ImGui::InputText("##args", &s_uiConfig->diff.args, 1024, flags);
+				ImGui::PopItemWidth();
 			}
-			PopID();
+			ImGui::PopID();
 		}
 		if(ImGui::CollapsingHeader("Perforce", ImGuiTreeNodeFlags_DefaultOpen)) {
-			PushID("Perforce");
+			ImGui::PushID("Perforce");
 			ImGui::AlignFirstTextHeightToWidgets();
 			ImGui::TextUnformatted("Changelists to fetch at a time:");
 			ImGui::SameLine();
 			int val = (int)s_uiConfig->p4.changelistBlockSize;
-			InputInt("##changelistBlockSize", &val, 100, 1000);
+			ImGui::InputInt("##changelistBlockSize", &val, 100, 1000);
 			val = BB_CLAMP(val, 0, 10000);
 			s_uiConfig->p4.changelistBlockSize = (u32)val;
 			ImGui::SameLine();
 			ImGui::TextUnformatted("(0 fetches all)");
-			PopID();
+			ImGui::PopID();
 		}
-		Separator();
-		if(Button("Ok")) {
+		ImGui::Separator();
+		if(ImGui::Button("Ok")) {
 			config_t tmp = *config;
 			*config = *s_uiConfig;
 			*s_uiConfig = tmp;
 			s_configOpen = false;
-			GetIO().MouseDoubleClickTime = config->doubleClickSeconds;
+			ImGui::GetIO().MouseDoubleClickTime = config->doubleClickSeconds;
 			QueueUpdateDpiDependentResources();
 		}
-		SameLine();
-		if(Button("Cancel")) {
+		ImGui::SameLine();
+		if(ImGui::Button("Cancel")) {
 			s_configOpen = false;
 			UIConfig_ApplyColorscheme();
 		}
 	}
-	End();
+	ImGui::End();
 	if(!s_configOpen) {
 		config_free(s_uiConfig);
 		s_uiConfig = nullptr;
